3f4b8290d6f318a357d49a5fd16312d0f0150c9b
fbettencourt22 <fsbettencourt22@gmail.com>
2025-07-23 12:55:08 +0100

    autoestrada

--- Calcular_Multas.py
+++ Calcular_Multas.py
@@ -15,3 +15,13 @@ def multa_fora_localidade(velocidade):
         return 120
     else:
         return 60
+    
+def multa_autoestrada(velocidade):
+    if velocidade <= 120:
+        return 0
+    elif velocidade > 175:
+        return 360
+    elif velocidade > 150:
+        return 120
+    else:
+        return 60
